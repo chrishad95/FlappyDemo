c1d9d8c56a8680bf53d2dcec99e842996cbae9a2
chrishad95 <chrishadley@gmail.com>
2015-10-05 14:41:47 -0400

    Finished Part 6.

---
+++ core/src/com/taylorbest/flappydemo/sprites/Bird.java
@@ -0,0 +1,38 @@
+package com.taylorbest.flappydemo.sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector3;
+import com.taylorbest.flappydemo.Game;
+
+/**
+ * Created by chadley on 10/5/2015.
+ */
+public class Bird {
+    private final static int GRAVITY = -15;
+    private Vector3 position;
+    private Vector3 velocity;
+    private Texture bird;
+
+    public Vector3 getPosition() {
+        return position;
+    }
+
+    public Texture getBird() {
+        return bird;
+    }
+
+    public Bird(int x, int y) {
+        position = new Vector3(x, y , 0);
+        velocity = new Vector3(0, 0 , 0);
+        bird = new Texture("bird.png");
+
+    }
+
+    public void update(float dt) {
+        velocity.add(0, GRAVITY, 0);
+        velocity.scl(dt);
+        position.add(0, velocity.y, 0);
+        velocity.scl(1/dt);
+
+    }
+}
--- core/src/com/taylorbest/flappydemo/states/PlayState.java
+++ core/src/com/taylorbest/flappydemo/states/PlayState.java
@@ -3,27 +3,31 @@ package com.taylorbest.flappydemo.states;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.taylorbest.flappydemo.Game;
+import com.taylorbest.flappydemo.sprites.Bird;
 
 /**
  * Created by chadley on 10/5/2015.
  */
 public class PlayState extends State {
 
-    private Texture bird;
+    private Bird bird;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
-        bird = new Texture("bird.png");
+        bird = new Bird(50,100);
+
         cam.setToOrtho(false, Game.WIDTH/2, Game.HEIGHT/2);
     }
 
     @Override
     public void handleInput() {
-
     }
 
     @Override
     public void update(float dt) {
+
+        handleInput();
+        bird.update(dt);
 
     }
 
@@ -31,7 +35,7 @@ public class PlayState extends State {
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
-        sb.draw(bird, 50, 50);
+        sb.draw(bird.getBird(), bird.getPosition().x, bird.getPosition().y);
         sb.end();
 
     }
